9819f08f0151f48c73448132fa48def4680f9cf3
zen-h01 <hovandat0.1@hotmail.com>
2021-06-08 09:16:36 +0700

    news and weather

--- source/voiceAssistant.py
+++ source/voiceAssistant.py
@@ -1,13 +1,54 @@
 from datetime import date, datetime
+from bs4.element import TemplateString
+from bs4 import BeautifulSoup
+from unidecode import unidecode
 import speech_recognition
 import pyttsx3
 import wikipedia
+import requests
 import os
 import subprocess
 
 robotEar = speech_recognition.Recognizer()													# robotEar: bien nhan am thanh
 robotMouth = pyttsx3.init()																	# robotMoutth: bien phat am thanh
 robotBrain = ""																				# robotBrain: bien nhan noi dung phan hoi lai
+
+def getWeather(str):
+	place = str.split("weather")[1]
+	search = f"temperature{place}"
+	url = f"https://www.google.com/search?q={search}"
+	r = requests.get(url)
+	data = BeautifulSoup(r.text, "html.parser")
+	temp = data.find("div",class_="BNeawe").text
+	return temp
+
+def getNews():
+	#import what we need
+	from requests_html import HTMLSession
+	session = HTMLSession()
+	#use session to get the page
+	r = session.get('https://news.google.com/topstories?hl=vi&gl=VN&ceid=VN%3Avi')
+	#render the html, sleep=1 to give it a second to finish before moving on. scrolldown= how many times to page down on the browser, to get more results. 5 was a good number here
+	r.html.render(sleep=1, scrolldown=0)
+	#find all the articles by using inspect element and create blank list
+	articles = r.html.find('article')
+	newslist = []
+	#loop through each article to find the title and link. try and except as repeated articles from other sources have different h tags.
+	for item in articles:
+	    try:
+	        newsitem = item.find('h3', first=True)
+	        title = newsitem.text
+	        link = newsitem.absolute_links
+	        newsarticle = {
+	            'title': unidecode(title),
+	            'link': link 
+	        }
+	        newslist.append(unidecode(title))
+	    except:
+	       pass
+	#print the length of the list
+	for i in range(5):
+	    print(newslist[i], end = '\n')
 
 def openApp(name):								#hàm tìm đường dẫn và mở app
 	appPath = ""
@@ -63,6 +104,11 @@ while True:
 			openApp("msedge.exe")
 		elif "chrome" in you:
 			openApp("chrome.exe")
+	elif "weather" in you:
+		robotBrain = "The weather is: " + getWeather(you)
+	elif "news" in you:
+		robotBrain = "Here's you news"
+		getNews()
 	elif "bye" in you:
 		robotBrain = "Good bye, Toan"
 		voices = robotMouth.getProperty('voices')  
